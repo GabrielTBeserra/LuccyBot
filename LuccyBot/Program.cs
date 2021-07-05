7c35dc3620e17010c17ad28e5bd0aa20c16549f2
SMARAPD - Gabriel Beserra <GTBESERRA@SMARAPD.COM.BR>
2021-07-05 17:33:15 -0300

    closed issue #1

---
+++ LuccyBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuccyBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
